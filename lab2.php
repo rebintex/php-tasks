8bf4dcd00f688f2557433214ec445d6a08db0604
rebintex <rebintex@mail.ru>
2022-01-18 00:05:49 +0500

    Second lab

---
+++ lab2.php
@@ -0,0 +1,95 @@
+<?php
+
+//------------------------ Task 1
+// $a = readline();
+
+// if($a % 2 == 0) {
+// echo "$a is even number";
+// } else {
+// echo "$a is odd number";
+// }
+
+//------------------------ Task 2
+
+// $a = readline();
+// $b = readline();
+// $c = readline();
+
+// if($a == $b && $a == $c && $b == $c) {
+// 	echo "The numbers have the same value";
+// } elseif ($a == $b || $a == $c || $b == $c) {
+// 	echo "Only two numbers have the same value";
+
+// } else {
+// 	echo "All numbers have different value";
+
+// }
+
+//------------------------ Task 3
+// $first = readline();
+// $second = readline();
+// $third = readline();
+
+// if($first > $second && $first > $third) {
+// 	echo "The first value $first is the largest one.";
+
+// } elseif($second > $first && $second > $third) {
+// 	echo "The second value $second is the largest one.";
+
+// } elseif($third > $first && $third > $second) {
+// 	echo "The third value $third is the largest one.";
+// } else {
+// 	echo "All values are equal.";
+// }
+
+//------------------------ Task 4
+// $operator = readline();
+// $operand1 = readline();
+// $operand2 = readline();
+
+// $res = 0;
+
+// if($operator == '+') {
+// $res = $operand1 + $operand2;
+// 	echo "Operation result: " .$operand1. " + " .$operand2 ." = ". $res;
+// } elseif($operator == '-') {
+// $res = $operand1 - $operand2;
+// 	echo "Operation result: " .$operand1. " - " .$operand2 ." = ". $res;
+// } elseif($operator == '*') {
+// $res = $operand1 * $operand2;
+// 	echo "Operation result: " .$operand1. " * " .$operand2 ." = ". $res;
+// } elseif($operator == '/') {
+// $res = $operand1 / $operand2;
+// 	echo "Operation result: " .$operand1. " / " .$operand2 ." = ". $res;
+// } else {
+// 	echo "Insert the correct operator!";
+// }
+
+//------------------------ Task 5
+
+
+//---------------------------------
+
+
+//------------------------ Task 6
+// $letter = readline();
+
+// if($letter == 'a' || $letter == 'o' || $letter == 'e' || $letter == 'i' || $letter ==  'u') {
+// 	echo "It's a vowel";
+// } else{
+// 	echo "It's a consonant";
+// }
+
+// ------------------------ Task 7
+
+// $leapYear = readline();
+
+// if((substr($leapYear, -2) == 00 && $leapYear % 400 == 0) || ($leapYear % 4 == 0 && substr($leapYear, -2) != 00 )) {
+// 	echo "$leapYear is a LEAP year!";
+// }else {
+// 	echo "$leapYear is NOT a leap year!";
+// }
+
+
+
+
